8ef1c11b01b4e40fd7423bdcadc9504ea9087f53
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-05-22 15:54:39 +0200

    update client sequelize and .env.example

--- packages/backend/models/models.js
+++ packages/backend/models/models.js
@@ -1,7 +1,14 @@
 import "dotenv/config";
 import { Sequelize, DataTypes, Model } from 'sequelize';
 
-export const sequelize = new Sequelize(process.env.PG_URL);
+export const sequelize = new Sequelize({
+    dialect: process.env.DATABASE_DIALECT ,
+    host: process.env.DATABASE_HOST,
+    port: process.env.DATABASE_PORT,
+    database: process.env.DATABASE_NAME ,
+    username: process.env.DATABASE_USER ,
+    password: process.env.DATABASE_PASSWORD ,
+});
 
 export class User extends Model {}
 export class List extends Model {}
