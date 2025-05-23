93952085469c3a4d3500483bef9e14eb921859fe
KannannB <kannann.bhanlin@gmail.com>
2025-05-23 12:25:18 +0200

    Mise en route de l'app

--- packages/backend/models/models.js
+++ packages/backend/models/models.js
@@ -1,13 +1,13 @@
 import "dotenv/config";
-import { Sequelize, DataTypes, Model } from 'sequelize';
+import { Sequelize, DataTypes, Model } from "sequelize";
 
 export const sequelize = new Sequelize({
-    dialect: process.env.DATABASE_DIALECT ,
-    host: process.env.DATABASE_HOST,
-    port: process.env.DATABASE_PORT,
-    database: process.env.DATABASE_NAME ,
-    username: process.env.DATABASE_USER ,
-    password: process.env.DATABASE_PASSWORD ,
+  dialect: process.env.DATABASE_DIALECT,
+  host: process.env.DATABASE_HOST,
+  port: process.env.DATABASE_PORT,
+  database: process.env.DATABASE_NAME,
+  username: process.env.DATABASE_USER,
+  password: process.env.DATABASE_PASSWORD,
 });
 
 export class User extends Model {}
@@ -18,135 +18,134 @@ export class Genre extends Model {}
 export class Book extends Model {}
 export class BookList extends Model {}
 
-
-User.init({
+User.init(
+  {
     lastname: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     firstname: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     email: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
     },
     password: {
-        type: DataTypes.STRING(255),
-        allowNull: false
-    }
-}, { sequelize,
-    tableName: 'user',
-    freezeTableName: true
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+  },
+  { sequelize, tableName: "user", timestamps: false, freezeTableName: true }
+);
 
- });
-
-List.init({
+List.init(
+  {
     name: {
-        type: DataTypes.STRING(255),
-        allowNull:false
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     position: {
-        type: DataTypes.INTEGER,
-        allowNull: false
-    }
-}, { sequelize,
-    tableName: 'list',
-    freezeTableName: true
- });
+      type: DataTypes.INTEGER,
+      allowNull: false,
+    },
+  },
+  { sequelize, tableName: "list", timestamps: false, freezeTableName: true }
+);
 
-Author.init({
+Author.init(
+  {
     lastname: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     firstname: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
+      type: DataTypes.STRING(255),
+      allowNull: true,
     },
     nationality: {
-        type: DataTypes.STRING(255),
-        allowNull: true,
-    }
-}, { sequelize,
-    tableName: 'author'
- });
-
-Editor.init({
-    name: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: true,
     },
-}, { sequelize,
-    tableName: 'editor',
-    freezeTableName: true
- });
+  },
+  { sequelize, timestamps: false, tableName: "author" }
+);
 
-Genre.init({
+Editor.init(
+  {
     name: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
-}, { sequelize,
-    tableName: 'genre',
-    freezeTableName: true
- });
+  },
+  { sequelize, timestamps: false, tableName: "editor", freezeTableName: true }
+);
 
-Book.init({
+Genre.init(
+  {
+    name: {
+      type: DataTypes.STRING(255),
+      allowNull: false,
+    },
+  },
+  { sequelize, tableName: "genre", timestamps: false, freezeTableName: true }
+);
+
+Book.init(
+  {
     title: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING(255),
+      allowNull: false,
+      unique: true,
     },
     isbn: {
-        type: DataTypes.STRING(20),
-        allowNull: false,
-        unique: true
+      type: DataTypes.STRING(20),
+      allowNull: false,
+      unique: true,
     },
     year_published: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
+      type: DataTypes.INTEGER,
+      allowNull: true,
     },
     description: {
-        type: DataTypes.STRING(255),
-        allowNull: false,
+      type: DataTypes.STRING(255),
+      allowNull: false,
     },
     pages: {
-        type: DataTypes.INTEGER,
-        allowNull: true,
-    }
-}, { sequelize,
-    tableName: 'book',
-    freezeTableName: true
- });
+      type: DataTypes.INTEGER,
+      allowNull: true,
+    },
+  },
+  { sequelize, tableName: "book", timestamps: false, freezeTableName: true }
+);
 
-BookList.init({
+BookList.init(
+  {
     book_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        unique: 'book_list_unique',
-        references: {
-            model: Book,
-            key: 'id'
-        }
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: "book_list_unique",
+      references: {
+        model: Book,
+        key: "id",
+      },
     },
     list_id: {
-        type: DataTypes.INTEGER,
-        allowNull: false,
-        unique: 'book_list_unique',
-        references: {
-            model: List,
-            key: 'id'
-        }
-    }
-}, { sequelize,
-    tableName: 'booklist',
-    freezeTableName: true
- });
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      unique: "book_list_unique",
+      references: {
+        model: List,
+        key: "id",
+      },
+    },
+  },
+  { sequelize, tableName: "booklist", timestamps: false, freezeTableName: true }
+);
 
 // BookList.associate = (models) => {
 //     BookList.belongsTo(models.Book, { foreignKey: 'bookId', onDelete: 'CASCADE' });
 //     BookList.belongsTo(models.List, { foreignKey: 'listId', onDelete: 'CASCADE' });
-// }; 
+// };
--- packages/backend/routers/bookListRouter.js
+++ packages/backend/routers/bookListRouter.js
@@ -1,8 +1,9 @@
-import { Router } from 'express';
+import { Router } from "express";
+import catchErrors from "../middlewares/catchErrors.js";
 const bookListRouter = Router();
 
-import { bookListController } from '../controllers/bookListController.js';
+import { bookListController } from "../controllers/bookListController.js";
 
-bookListRouter.get('/lists', bookListController.index);
+bookListRouter.get("/lists", catchErrors(bookListController.index));
 
 export { bookListRouter };
