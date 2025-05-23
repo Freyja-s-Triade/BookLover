45935f76ce937af11be7a1e2cba4b06b523774f0
KannannB <kannann.bhanlin@gmail.com>
2025-05-23 14:11:23 +0200

    test

--- packages/backend/routers/index.js
+++ packages/backend/routers/index.js
@@ -1,12 +1,12 @@
-import { Router } from 'express';
-import { bookListRouter } from './bookListRouter.js'
+import { Router } from "express";
+import { bookListRouter } from "./bookListRouter.js";
 
 export const router = Router();
 
 //test
 /*
 router.get('/', (req, res) => {
-    res.status(200).json({message: 'Le serveur est lancé!'});
+    res.status(200).json({message: 'Le serveur est lancé!'}); /test
 });
 */
 
