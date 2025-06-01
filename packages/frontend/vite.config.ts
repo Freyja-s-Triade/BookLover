4cc4c82739f5883d774aa4b5e8442670a5e7c7cb
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-06-01 14:54:26 +0200

    add config tailwind and daisyUI

--- packages/frontend/vite.config.ts
+++ packages/frontend/vite.config.ts
@@ -2,10 +2,11 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 import svgr from "vite-plugin-svgr";
 import { fileURLToPath } from "node:url";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-    plugins: [react(), svgr()],
+    plugins: [react(), svgr(), tailwindcss()],
     css: {
         preprocessorOptions: {
             scss: {
