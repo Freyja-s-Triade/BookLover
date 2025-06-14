1788faae3680af291dcba4f10843f4403dbe261f
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-06-14 12:14:27 +0200

    add types

---
+++ packages/frontend/src/@types/index.d.ts
@@ -0,0 +1,62 @@
+export type User = {
+    id: number;
+    lastname: string;
+    firstname: string;
+    email: string;
+    password: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
+
+export type BookList = {
+    id: number;
+    bookId: number;
+    ListId: number;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
+
+export type List = {
+    id: number;
+    name: string;
+    position: number;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
+
+export type Author = {
+    id: number;
+    lastname: string;
+    firstname: string | null;
+    nationality: string | null;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
+
+export type Editor = {
+    id: number;
+    name: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
+
+export type Genre = {
+    id: number;
+    name: string;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
+
+export type Book = {
+    id: number;
+    title: string;
+    isbn: string;
+    year_published: number | null;
+    description: string | null;
+    pages: number | null;
+    AuthorId: number | null;
+    GenreId: number | null;
+    EditorId: number | null;
+    CreatedAt: string;
+    UpdatedAt: string;
+};
--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,11 +1,20 @@
+import { Routes, Route } from "react-router";
+
 import Header from "./components/header/header";
 import Footer from "./components/footer/footer";
+
+import HomePage from "./pages/HomePage";
 
 function App() {
     return (
         <div className="flex flex-col h-screen">
             <Header />
-            <main className="flex-grow">TODO</main>
+            <main className="flex-grow">
+                TODO
+                <Routes>
+                    <Route path="/" element={<HomePage />} />
+                </Routes>
+            </main>
             <Footer />
         </div>
     );
