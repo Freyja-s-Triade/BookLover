05654d25ed73363c4504304d8365a02413080f1c
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-06-14 11:50:10 +0200

    refacto header and create footer

--- packages/frontend/src/App.tsx
+++ packages/frontend/src/App.tsx
@@ -1,10 +1,12 @@
 import Header from "./components/header/header";
+import Footer from "./components/footer/footer";
 
 function App() {
     return (
         <div className="flex flex-col h-screen">
-            <Header className="flex items-center justify-between" />
+            <Header />
             <main className="flex-grow">TODO</main>
+            <Footer />
         </div>
     );
 }
--- packages/frontend/src/components/header/header.tsx
+++ packages/frontend/src/components/header/header.tsx
@@ -1,69 +1,59 @@
 import { NavLink } from "react-router";
-import { useState } from "react";
 
 import logo from "@src/assets/icons/logo.svg";
 
-interface HeaderProps {
-    className?: string;
-}
-
-export default function Header({ className = "" }: HeaderProps) {
-    // useState to manage the display of the burger menu
-    const [isOpen, setIsOpen] = useState(false);
-
-    // change the display of the burger menu
-    const toggleMenu = () => {
-        setIsOpen(!isOpen);
-    };
-
+export default function Header() {
     return (
-        <header className={`p-4 ${className}`}>
-            {/* logo */}
-            <a href="/" className="logo">
-                <img src={logo} alt="Logo BookLover" />
-            </a>
+        <header>
+            <div className="navbar w-full bg-base-100 shadow-sm">
+                {/* burger menu */}
+                <div className="navbar-start">
+                    <div className="dropdown">
+                        <div tabIndex={0} role="button" className="btn btn-ghost btn-circle">
+                            {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                {" "}
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h7"
+                                />{" "}
+                            </svg>
+                        </div>
+                        <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-right">
+                            <li className="text-right">
+                                <NavLink to="lists" className="nav-links block truncate">
+                                    Mes listes
+                                </NavLink>
+                            </li>
+                            <li className="text-right">
+                                <NavLink to="tags" className="nav-links block truncate">
+                                    Tags
+                                </NavLink>
+                            </li>
+                            <li className="text-right">
+                                <NavLink to="account" className="nav-links block truncate">
+                                    Mon compte
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
 
-            {/* burger menu */}
-            <nav>
-                {/* biome-ignore lint/a11y/useButtonType: <explanation> */}
-                <button onClick={toggleMenu}>
-                    {isOpen ? (
-                        // cross icon
-                        // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
-                            <line x1="10" y1="10" x2="40" y2="40" stroke="#000" strokeWidth="4" strokeLinecap="round" />
-                            <line x1="40" y1="10" x2="10" y2="40" stroke="#000" strokeWidth="4" strokeLinecap="round" />
-                        </svg>
-                    ) : (
-                        // menu burger icon
-                        // biome-ignore lint/a11y/noSvgWithoutTitle: <explanation>
-                        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 50 50" width="50px" height="50px">
-                            <path d="M5 8A2 2 0 1 0 5 12L45 12A2 2 0 1 0 45 8L5 8zM5 23A2 2 0 1 0 5 27L45 27A2 2 0 1 0 45 23L5 23zM5 38A2 2 0 1 0 5 42L45 42A2 2 0 1 0 45 38L5 38z" />
-                        </svg>
-                    )}
-                </button>
-
-                {/* links are displayed on click on the burger menu */}
-                {isOpen && (
-                    <ul className="flex flex-col text-right">
-                        <li>
-                            <NavLink to="lists" className="nav-links">
-                                Mes listes
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="tags" className="nav-links">
-                                Tags
-                            </NavLink>
-                        </li>
-                        <li>
-                            <NavLink to="account" className="nav-links">
-                                Mon compte
-                            </NavLink>
-                        </li>
-                    </ul>
-                )}
-            </nav>
+                {/* logo */}
+                <div className="navbar-end">
+                    <a href="/" className="logo">
+                        <img src={logo} alt="Logo BookLover" />
+                    </a>
+                </div>
+            </div>
         </header>
     );
 }
