b1ab437c66fec9797bd156e2b380707dc05e24b4
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-07-04 09:50:41 +0200

    HomePage

--- packages/frontend/src/pages/HomePage.tsx
+++ packages/frontend/src/pages/HomePage.tsx
@@ -1,9 +1,32 @@
+import type { HomePageList } from "../@types";
 import { fetchHomePageLists } from "../services/api";
 import { useQuery } from "@tanstack/react-query";
 
 export default function HomePage() {
-    const { isPending, error, data } = useQuery({
+    const {
+        isPending,
+        error,
+        data: lists,
+    } = useQuery<HomePageList[]>({
         queryKey: ["repoData"],
         queryFn: () => fetchHomePageLists(),
     });
+
+    if (isPending) return "Loading";
+
+    if (error) return `An error has occured: ${error.message}`;
+
+    return (
+        <div>
+            <h1>Mes listes</h1>
+
+            <ul>
+                {lists.map((list) => (
+                    <li key={list.id}>
+                        <HomePageListCard list={list} />
+                    </li>
+                ))}
+            </ul>
+        </div>
+    );
 }
