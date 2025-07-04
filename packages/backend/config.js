7fba5f96bd26f3e1983941978131fdef852eff40
ChloeGarciaMillerand <chloe@garcia.cloud>
2025-07-04 11:12:50 +0200

    staged

--- packages/backend/config.js
+++ packages/backend/config.js
@@ -3,5 +3,9 @@ import "dotenv/config.js";
 export const server = {
     protocol: process.env.PROTOCOL,
     host: process.env.HOST,
-    port: process.env.PORT
+    port: process.env.PORT,
+    cors: {
+        allowedDomains: process.env.CORS_ALLOWED_DOMAINS || ["http://localhost:5173", "http://127.0.0.1:5173"],
+        credentials: process.env.CORS_CREDENTIALS || true
+    },
 };
