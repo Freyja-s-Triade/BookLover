eca78cb0e7b3708a3f316054c3ade46742d2e36f
Chloé Garcia <193918132+ChloeGarciaMillerand@users.noreply.github.com>
2025-08-13 15:03:48 +0200

    homepage with react router

---
+++ packages/front-end-react-router/app/components/header.tsx
@@ -0,0 +1,59 @@
+import { NavLink } from "react-router";
+
+import logo from "@src/assets/icons/logo.svg";
+
+export default function Header() {
+    return (
+        <header>
+            <div className="navbar w-full bg-base-100 shadow-sm">
+                {/* burger menu */}
+                <div className="navbar-start">
+                    <a href="/" className="logo">
+                        <img src={logo} alt="Logo BookLover" />
+                    </a>
+                </div>
+
+                {/* logo */}
+                <div className="navbar-end">
+                    <div className="dropdown dropdown-end">
+                        <button type="button" className="btn btn-ghost btn-circle">
+                            {/* biome-ignore lint/a11y/noSvgWithoutTitle: <explanation> */}
+                            <svg
+                                xmlns="http://www.w3.org/2000/svg"
+                                className="h-5 w-5"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                            >
+                                {" "}
+                                <path
+                                    strokeLinecap="round"
+                                    strokeLinejoin="round"
+                                    strokeWidth="2"
+                                    d="M4 6h16M4 12h16M4 18h7"
+                                />{" "}
+                            </svg>
+                        </button>
+                        <ul className="menu menu-sm dropdown-content bg-base-100 rounded-box z-1 mt-3 w-52 p-2 shadow text-right">
+                            <li className="text-right">
+                                <NavLink to="lists" className="nav-links block truncate">
+                                    Mes listes
+                                </NavLink>
+                            </li>
+                            <li className="text-right">
+                                <NavLink to="tags" className="nav-links block truncate">
+                                    Tags
+                                </NavLink>
+                            </li>
+                            <li className="text-right">
+                                <NavLink to="account" className="nav-links block truncate">
+                                    Mon compte
+                                </NavLink>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </header>
+    );
+}
---
+++ packages/front-end-react-router/app/routes/home.tsx
@@ -0,0 +1,35 @@
+import { Link } from "react-router";
+import HomePageListCard from "../components/HomePageListCard";
+import { Button } from "../components/button";
+import { fetchHomePageLists } from "../services/api";
+import type { Route } from "./+types/home";
+
+export async function loader() {
+    const lists = await fetchHomePageLists();
+
+    return { lists };
+}
+
+export default function HomePage(props: Route.ComponentProps) {
+    const { lists } = props.loaderData;
+
+    return (
+        <div className="m-auto w-4/5 mt-4">
+            <h1 className="h1">Mes listes</h1>
+
+            <ul>
+                {lists.map((list) => (
+                    <li key={list.id}>
+                        <HomePageListCard list={list} />
+                    </li>
+                ))}
+            </ul>
+
+            <div className="flex justify-end">
+                <Link to="/addList">
+                    <Button className="btn-success">+</Button>
+                </Link>
+            </div>
+        </div>
+    );
+}
